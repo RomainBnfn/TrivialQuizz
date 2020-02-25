a60dfa87ced206f3e1a73e18326822ff4a964dee
RomainBnfn <rbonnefon@esnc.fr>
2020-02-25 16:03:38 +0100

    Debut

--- trivialquizz/index.php
+++ trivialquizz/index.php
@@ -1,3 +1,11 @@
-<?php
-  echo "TEST";
-?>
+<!doctype html>
+<html lang="fr">
+<head>
+  <title>Trivial Quizz</title>
+  <?php require_once "include/header.html"?>
+</head>
+<body>
+  <!-- Le reste du contenu -->
+
+</body>
+</html>
