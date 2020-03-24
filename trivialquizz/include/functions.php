104a996b92002dfbe264a54eb187b426872e5331
Paul Lorgue <paullorgue@gmail.com>
2020-03-24 14:31:15 +0100

    commentage de fonction.php et fusion avec function.php

--- trivialquizz/include/functions.php
+++ trivialquizz/include/functions.php
@@ -4,7 +4,7 @@
 //  Ces fonctions lancent des Requêtes SQL :
 
   /// Essaie de charger un quizz, envoie null s'il n'existe pas.
-  /// S'il existe, renvoie un tableau contenant toutes ses infos.
+  /// S'il existe, renvoie un tableau contenant toutes ses infos (id, nom,desc,id_theme).
   function tryLoadQuizz($bdd, $idQuizz)
   {
     try
@@ -39,7 +39,7 @@
   }
 
   /// Essaie de charger un theme, envoie null s'il n'existe pas.
-  /// S'il existe, renvoie un tableau contenant toutes ses infos.
+  /// S'il existe, renvoie un tableau contenant toutes ses infos (id, nom, couleur).
   function tryLoadTheme($bdd, $idTheme)
   {
     try
@@ -72,7 +72,7 @@
 
   /// Essaie de charger toutes les questions d'un quizz, puis renvoie une liste
   /// de liste qui comportent les infos des questions. Renvoie null sinon.
-  ///   Renvoie $_QUESTIONS[] qui comporte tous les $_QUESTION[]
+  /// Renvoie $_QUESTIONS[] qui comporte tous les $_QUESTION[] (id, lib, id_bonneRep)
   function tryLoadQuizzQuestion($bdd, $idQuizz)
   {
     try
@@ -110,7 +110,7 @@
 
   /// Essaie de charger tous les thèmes, puis renvoie une liste de listes
   /// qui comportent les infos des thèmes. Renvoie null sinon.
-  ///   Renvoie $_THEMES[] qui comporte tous les $_THEME[]
+  ///   Renvoie $_THEMES[] qui comporte tous les $_THEME[] (id, nom, couleur, desc)
   function getAllThemesInfos($bdd)
   {
     $requete = $bdd -> query("SELECT * FROM theme");
@@ -137,7 +137,7 @@
 
   /// Essaie de charger tous les quizzes, puis renvoie une liste de listes
   /// qui comportent les infos des thèmes. Renvoie null sinon.
-  ///   Renvoie $_QUIZZES[] qui comporte tous les $_QUIZZ[]
+  ///   Renvoie $_QUIZZES[] qui comporte tous les $_QUIZZ[] (id, nom, desc, id_theme)
   function getAllQuizzesInfos($bdd)
   {
     $requete = $bdd -> query("SELECT * FROM quiz ORDER BY th_id");
@@ -204,6 +204,55 @@
     }
     return $listeNoms;
   }
+  // -------------------------------------------------------------------
+  //  fonctions pour la construction du camembert des thèmes sur la page d'Accueil:
+
+
+  //renvoie les path svg du camembert tableau de dim 6 (6 parts)
+  //$r marge entre les "part", $R rayon d'une "part", $c centre de la roue/"du gateau"
+  function generatePath($r, $R, $c){
+    $A = array( $c-$r*cos(toRad(60)), $c-$r*sin(toRad(60)));
+    $B = array( $c+$r*cos(toRad(60)), $A[1]);
+    $C = array( $c+$r, $c);
+    $D = array( $B[0], $c+$r*sin(toRad(60)));
+    $E = array( $A[0], $D[1]);
+    $F = array( $c-$r, $c);
+
+    $a1x = $A[0]-$R*cos(toRad(30)); $a1y = $A[1]-$R*sin(toRad(30));
+    $a2y = $A[1]-$R;
+    $b2x = $B[0]+$R*cos(toRad(30));
+    $c1x = $C[0]+$R*cos(toRad(30)); $c1y = $C[1]-$R*sin(toRad(30));
+    $c2y = $C[1]+$R*sin(toRad(30));
+    $d1y = $D[1]+$R*sin(toRad(30));
+    $d2y = $D[1]+$R;
+    $f1x = $F[0]-$R*cos(toRad(30));
+
+    $l = $R+$r;
+
+    $path = array();
+    $path[0] = "M $a1x,$a1y A $l,$l 0 0 1 $A[0],$a2y L $A[0],$A[1] Z";
+    $path[1] = "M $B[0],$a2y A $l,$l 0 0 1 $b2x,$a1y L $B[0],$B[1] Z";
+    $path[2] = "M $c1x,$c1y A $l,$l 0 0 1 $c1x,$c2y L $C[0],$C[1] Z";
+    $path[3] = "M $b2x,$d1y A $l,$l 0 0 1 $B[0],$d2y L $D[0],$D[1] Z";
+    $path[4] = "M $A[0],$d2y A $l,$l 0 0 1 $a1x,$d1y L $E[0],$E[1] Z";
+    $path[5] = "M $f1x,$c2y A $l,$l 0 0 1 $f1x,$c1y L $F[0],$F[1] Z";
+
+    return $path;
+  }
+
+  //Renvoie un tableau de coordonnées (X Y) de position des titres sur le camembert des themes
+  function generateCoordText($r, $R, $c){
+    return
+    array(
+      array($c-0.9*$R*cos(toRad(34)),$c-0.9*$R*sin(toRad(37))),
+      array($c+0.7*$R*cos(torad(80)),$c-0.9*$R*sin(toRad(37))),
+      array($c+0.2*$R,$c+0.03*$R),
+      array($c+0.7*$R*cos(torad(85)),$c+0.9*$R*sin(toRad(40))),
+      array($c-0.9*$R*cos(toRad(33)),$c+0.9*$R*sin(toRad(40))),
+      array($c-0.9*$R,$c+0.03*$R)
+    );
+  }
+
   // -----------------------------------------------------------------------------
   //  Autre :
 
@@ -223,4 +272,10 @@
     }
     return null;
   }
+  //Convertie des degrès en rad
+  function toRad($deg){
+    return $deg*pi()/180;
+  }
+
+
 ?>
