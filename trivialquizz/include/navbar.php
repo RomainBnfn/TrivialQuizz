1fe99e4ace5896102d22a1b3f53b5d25b7b13678
Paul Lorgue <paullorgue@gmail.com>
2020-03-31 17:38:27 +0200

    mise à jour de la navbar

--- trivialquizz/include/navbar.php
+++ trivialquizz/include/navbar.php
@@ -1,8 +1,27 @@
+<?php
+  //TODO: changer les liens
+
+
+  if(isset($_GET['unlog'])){
+    $_SESSION['pseudo']="";
+    $_SESSION['is_admin']="false";
+    header("Location: /trivial/trivialquizz/index.php");
+    exit();
+  }
+  $logo_link = "/trivial/trivialquizz/index.php";
+  $hello_txt = "Bonjour, ".$_SESSION['pseudo'];
+  if(isset($_SESSION['is_admin'])){
+    if($_SESSION['is_admin']=="true"){
+      $logo_link = "/trivial/trivialquizz/admin/index.php";
+      $hello_txt = "Bonjour, ".$_SESSION['pseudo']." (admin)";
+    }
+  }
+?>
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container">
     <!-- Logo pour aller à la page d'accueil -->
     <div>
-        <a href="/trivial/trivialquizz/index.php">
+        <a href="<?=$logo_link?>">
           ​<picture>
             <img src="/trivial/trivialquizz/image/logo.png" class="img-logo" alt="Logo du Trivial Quizz">
           </picture>
@@ -10,13 +29,30 @@
     </div>
 
     <!-- Se connecter -->
+    <?php
+      if(isset($_SESSION['pseudo'])){
+        if($_SESSION['pseudo'] != ''){
+    ?>
+      <div id="hello-navbar">
+        <p><?=$hello_txt?></p>
+        <a href="?unlog=true">
+          <button type="button" class="btn btn-outline-primary" name="unlog">Déconnexion</button>
+        </a>
+      </div>
+    <?php
+      }else{
+    ?>
     <div>
-      <a href="/trivial/trivialquizz/register.php"
+      <a href="/trivial/trivialquizz/register.php">
         <button type="button" class="btn btn-primary">Inscription</button>
       </a>
       <a href="/trivial/trivialquizz/log.php">
         <button type="button" class="btn btn-outline-primary">Connexion</button>
       </a>
     </div>
+    <?php
+        }
+      }
+    ?>
   </div>
 </nav>
